43f6c4cf8b1954fda9848de15aade5ba0555f4cb
matheusgevartosky <magevartosky@gmail.com>
2022-08-17 11:22:39 -0300

    fix: adjust user controller route

--- M2P2-DEVinCar/Controllers/UsersController.cs
+++ M2P2-DEVinCar/Controllers/UsersController.cs
@@ -2,37 +2,36 @@
 using Microsoft.AspNetCore.Mvc;
 
 
-
 namespace M2P2_DEVinCar.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/user")]
     [ApiController]
     public class UsersController : ControllerBase
     {
-            private DEVInCarContext _context;
-            private readonly ILogger<UsersController> _logger;
+        private DEVInCarContext _context;
+        private readonly ILogger<UsersController> _logger;
 
-            public UsersController(DEVInCarContext context
-                , ILogger<UsersController> logger)
-            {
-                _context = context;
-                _logger = logger;
-            }
+        public UsersController(DEVInCarContext context
+            , ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
 
-            [HttpGet]
+        /*[HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-       
+
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        
+
         [HttpPost]
         public void Post([FromBody] string value)
         {
@@ -42,6 +41,6 @@ namespace M2P2_DEVinCar.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-        }
+        }*/
     }
 }
