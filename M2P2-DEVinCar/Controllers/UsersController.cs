ae83426e34002ece79c46abc90004afd2027e86f
matheusgevartosky <magevartosky@gmail.com>
2022-08-17 10:49:38 -0300

    feat: Add user controller whitout any methods

---
+++ M2P2-DEVinCar/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using M2P2_DEVinCar.Context;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace M2P2_DEVinCar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+            private DEVInCarContext _context;
+            private readonly ILogger<UsersController> _logger;
+
+            public UsersController(DEVInCarContext context
+                , ILogger<UsersController> logger)
+            {
+                _context = context;
+                _logger = logger;
+            }
+
+            [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
