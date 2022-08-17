67d718821ec0a11be5161c31f888ccfc2c4369a7
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-08-17 13:55:40 -0300

    Add Model User

---
+++ M2P2-DEVinCar/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace M2P2_DEVinCar.Models
+{
+    [Table("User")]
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [MinLength(4)]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
