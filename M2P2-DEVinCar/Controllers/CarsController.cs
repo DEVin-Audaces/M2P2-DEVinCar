172f38683543ab20144e0d77180a7110de287454
matheusgevartosky <magevartosky@gmail.com>
2022-08-17 11:46:41 -0300

    feat: Add car controller whitout any methods

---
+++ M2P2-DEVinCar/Controllers/CarsController.cs
@@ -0,0 +1,43 @@
+﻿using M2P2_DEVinCar.Context;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace M2P2_DEVinCar.Controllers
+{
+    [Route("api/Car")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private DEVInCarContext _context;
+
+        public CarsController(DEVInCarContext context)
+        {
+            _context = context;
+        }
+
+        /*[HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }*/
+    }
+}
