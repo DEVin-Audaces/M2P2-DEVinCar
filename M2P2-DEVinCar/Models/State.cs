f764420e35850522134bc491f9e62c7098367026
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-08-17 20:54:06 -0300

    Creat State Model

---
+++ M2P2-DEVinCar/Models/State.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace M2P2_DEVinCar.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string Initials { get; set; }
+
+    }
+}
