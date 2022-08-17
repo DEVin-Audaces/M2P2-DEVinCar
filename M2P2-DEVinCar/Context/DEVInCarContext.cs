bfecbfaed2851c5f27bffcf279a6be0aa44ef384
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-08-17 14:51:06 -0300

    Add DbSet Users to Context

--- M2P2-DEVinCar/Context/DEVInCarContext.cs
+++ M2P2-DEVinCar/Context/DEVInCarContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using M2P2_DEVinCar.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace M2P2_DEVinCar.Context
 {
@@ -13,5 +14,6 @@ namespace M2P2_DEVinCar.Context
         {
         }
 
+        public DbSet<User> Users { get; set; }
     }
 }
