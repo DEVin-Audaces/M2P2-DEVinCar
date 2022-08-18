67429a9a6d4edceb7b054d7112d536e8ddbebb69
Vinicius Aloncio de Assunção <volutab617@gmail.com>
2022-08-17 21:16:52 -0300

    fix: Changing the name of CarModel to Car and removing the Require Data Annotation from id

---
+++ M2P2-DEVinCar/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace M2P2_DEVinCar.Models {
+    public class Car {
+        public int Id { get; set; }
+        [Required]
+        [Column("Nome")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [Column("PrecoSugerido")]
+        [StringLength(18, MinimumLength = 2)]
+        public double SuggestedPrice { get; set; }
+    }
+}
