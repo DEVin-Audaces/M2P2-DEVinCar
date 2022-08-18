66a9babf837442a1353815fcd5836349a43d8da7
Robson De Andrade <theairelibre@protonmail.com>
2022-08-18 17:26:04 -0300

    feat: Add SaleCar Entity

--- M2P2-DEVinCar/Models/SaleCar.cs
+++ M2P2-DEVinCar/Models/SaleCar.cs
@@ -1,25 +1,25 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace M2P2_DEVInCar.Models
+namespace M2P2_DEVinCar.Models
 {
     public class SaleCar
     {
     
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "SaleId is required.")]
+        [Required(ErrorMessage = "Campo SaleId de preenchimento obrigatório")]
         [ForeignKey("Sale")]
         public int SaleId { get; set; }
 
-        [Required(ErrorMessage = "CarId is required.")]
+        [Required(ErrorMessage = "Campo CarId de preenchimento obrigatório")]
         [ForeignKey("Car")]
         public int CarId { get; set; }
 
-        [Required(ErrorMessage = "UnitPrice is required.")]
+        [Required(ErrorMessage = "Campo UnitPrice de preenchimento obrigatório")]
         public decimal UnitPrice { get; set; }
 
-        [Required(ErrorMessage = "Amount is required.")]
+        [Required(ErrorMessage = "Campo Amount de preenchimento obrigatório")]
         public int Amount { get; set; }
 
         public Sale? Sale { get; set; }
