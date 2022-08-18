fffc809ceb17a1c17c467d56f1da88b38f1e1ea3
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-08-17 22:14:44 -0300

    feat: creating AddressesController

---
+++ M2P2-DEVinCar/Controllers/AddressesController.cs
@@ -0,0 +1,51 @@
+﻿using M2P2_DEVinCar.Context;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace M2P2_DEVinCar.Controllers
+{
+    [Route("api/address")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private DEVInCarContext _context;
+
+        public AddressesController(DEVInCarContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<AddressesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AddressesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AddressesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AddressesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AddressesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
