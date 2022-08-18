90c32d8b4b678a42062fb6bccf220647a6f46fdb
BCS93 <103464964+BCS93@users.noreply.github.com>
2022-08-18 20:13:56 -0300

    feat: Add Delivery entity

---
+++ M2P2-DEVinCar/Models/Delivery.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace M2P2_DEVinCar.Models
+{
+
+    public class Delivery
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Campo AddressId de preenchimento obrigatório")]
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        [Required(ErrorMessage = "Campo SaleId de preenchimento obrigatório")]
+        [ForeignKey("Sale")]
+        public int SaleId { get; set; }
+        [DataType(DataType.DateTime, ErrorMessage = "Formato de data inválido")]
+        public DateTime? DeliveryForecast { get; set; }
+        public Address? Address { get; set; }
+        public Sale? Sale { get; set; }
+
+    }
+}
