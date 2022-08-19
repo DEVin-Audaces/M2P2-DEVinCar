6c88df3aaf0ed70d4c62c0c67e0b598a894e2201
Vinicius Aloncio de Assunção <volutab617@gmail.com>
2022-08-19 00:20:42 -0300

    fix: Removing 'Column' data annotation

--- M2P2-DEVinCar/Models/Car.cs
+++ M2P2-DEVinCar/Models/Car.cs
@@ -5,11 +5,9 @@ namespace M2P2_DEVinCar.Models {
     public class Car {
         public int Id { get; set; }
         [Required]
-        [Column("Nome")]
         [StringLength(255)]
         public string Name { get; set; }
         [Required]
-        [Column("PrecoSugerido")]
         [Range(9, 100000000000000000, ErrorMessage = "O campo SuggestedPrice deve ser entre 9 e 100000000000000000.")]
         public double SuggestedPrice { get; set; }
     }
