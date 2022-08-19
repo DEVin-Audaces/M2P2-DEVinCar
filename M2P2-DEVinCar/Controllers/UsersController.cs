807f01317dac540b7c709bd47e7f9ad51c0853bb
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-08-19 19:39:21 -0300

    feat: Add Post Sales in UsersController

--- M2P2-DEVinCar/Controllers/UsersController.cs
+++ M2P2-DEVinCar/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using M2P2_DEVinCar.Context;
+using M2P2_DEVinCar.Dtos;
+using M2P2_DEVinCar.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace M2P2_DEVinCar.Controllers
@@ -9,10 +12,12 @@ namespace M2P2_DEVinCar.Controllers
     public class UsersController : ControllerBase
     {
         private DEVInCarContext _context;
+        private readonly ILogger<UsersController> _logger;
 
-        public UsersController(DEVInCarContext context)
+        public UsersController(DEVInCarContext context, ILogger<UsersController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         /*[HttpGet]
@@ -39,5 +44,55 @@ namespace M2P2_DEVinCar.Controllers
         public void Delete(int id)
         {
         }*/
+        
+                /// <summary>
+        /// Insert vendas.
+        /// </summary>
+        /// <param name="userId">ID do usuário.</param>
+        /// <param name="createSaleDTO">DTO de sales com BuyerId e SaleDate.</param>
+        /// <returns>Retorna venda inserida com sucesso no banco de dados .</returns>
+        /// <response code="201">venda inserida com sucesso.</response>
+        /// <response code="404">Inserção não realizada ou SellerId/BuyerId não encontrado no banco de dados.</response>
+        /// <response code="500">Ocorreu exceção durante a inserção.</response>
+        [HttpPost("{userId}/sales")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> PostSales(int userId, [FromBody] CreateSaleDto createSaleDTO)
+        {
+            try
+            {
+                var userIdSearch = await  _context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+            
+                if (userIdSearch is null)
+                    return StatusCode(404);    
+
+                userIdSearch = await  _context.Users.FirstOrDefaultAsync(x => x.Id == createSaleDTO.BuyerId);
+
+                if (userIdSearch is null)
+                    return StatusCode(404);    
+            
+                Sale sale = new();
+
+                sale.BuyerId = createSaleDTO.BuyerId;
+                sale.SellerId = userId;
+                sale.SaleDate = createSaleDTO.SaleDate == null ? DateTime.Now : createSaleDTO.SaleDate;
+
+            
+                _context.Sales.Add(sale);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation($"Controller:{nameof(UsersController)}-Method:{nameof(PostSales)}");
+
+                return CreatedAtAction(nameof(PostSales), new { Id = sale.Id }, sale.Id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Controller:{nameof(UsersController)}-Method:{nameof(PostSales)}");
+                return StatusCode(500);
+            }
+
+        }
+
     }
 }
