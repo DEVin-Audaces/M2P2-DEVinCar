02e1029b78e4b5ce80b784f8c59773598d460db2
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-08-20 12:32:15 -0300

    feat: add states-migration; changes context and snapshot

--- M2P2-DEVinCar/Context/DEVInCarContext.cs
+++ M2P2-DEVinCar/Context/DEVInCarContext.cs
@@ -18,6 +18,7 @@ namespace M2P2_DEVinCar.Context
         public DbSet<User> Users { get; set;}
         public DbSet<Sale> Sales { get; set; }
         public DbSet<SaleCar> SaleCars { get; set; }
+        public DbSet<State> States { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- M2P2-DEVinCar/Migrations/DEVInCarContextModelSnapshot.cs
+++ M2P2-DEVinCar/Migrations/DEVInCarContextModelSnapshot.cs
@@ -102,6 +102,193 @@ namespace M2P2_DEVinCar.Migrations
                     b.ToTable("SaleCars");
                 });
 
+            modelBuilder.Entity("M2P2_DEVinCar.Models.State", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<string>("Initials")
+                        .IsRequired()
+                        .HasMaxLength(2)
+                        .HasColumnType("nvarchar(2)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("States");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Initials = "AC",
+                            Name = "Acre"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Initials = "AL",
+                            Name = "Alagoas"
+                        },
+                        new
+                        {
+                            Id = 3,
+                            Initials = "AP",
+                            Name = "Amapá"
+                        },
+                        new
+                        {
+                            Id = 4,
+                            Initials = "AM",
+                            Name = "Amazonas"
+                        },
+                        new
+                        {
+                            Id = 5,
+                            Initials = "BA",
+                            Name = "Bahia"
+                        },
+                        new
+                        {
+                            Id = 6,
+                            Initials = "CE",
+                            Name = "Ceará"
+                        },
+                        new
+                        {
+                            Id = 7,
+                            Initials = "DF",
+                            Name = "Distrito Federal"
+                        },
+                        new
+                        {
+                            Id = 8,
+                            Initials = "ES",
+                            Name = "Espírito Santo"
+                        },
+                        new
+                        {
+                            Id = 9,
+                            Initials = "GO",
+                            Name = "Goiás"
+                        },
+                        new
+                        {
+                            Id = 10,
+                            Initials = "MA",
+                            Name = "Maranhão"
+                        },
+                        new
+                        {
+                            Id = 11,
+                            Initials = "MT",
+                            Name = "Mato Grosso"
+                        },
+                        new
+                        {
+                            Id = 12,
+                            Initials = "MS",
+                            Name = "Mato Grosso do Sul"
+                        },
+                        new
+                        {
+                            Id = 13,
+                            Initials = "MG",
+                            Name = "Minas Gerais"
+                        },
+                        new
+                        {
+                            Id = 14,
+                            Initials = "PA",
+                            Name = "Pará"
+                        },
+                        new
+                        {
+                            Id = 15,
+                            Initials = "PB",
+                            Name = "Paraíba"
+                        },
+                        new
+                        {
+                            Id = 16,
+                            Initials = "PR",
+                            Name = "Paraná"
+                        },
+                        new
+                        {
+                            Id = 17,
+                            Initials = "PE",
+                            Name = "Pernambuco"
+                        },
+                        new
+                        {
+                            Id = 18,
+                            Initials = "PI",
+                            Name = "Piauí"
+                        },
+                        new
+                        {
+                            Id = 19,
+                            Initials = "RJ",
+                            Name = "Rio de Janeiro"
+                        },
+                        new
+                        {
+                            Id = 20,
+                            Initials = "RN",
+                            Name = "Rio Grande do Norte"
+                        },
+                        new
+                        {
+                            Id = 21,
+                            Initials = "RS",
+                            Name = "Rio Grande do Sul"
+                        },
+                        new
+                        {
+                            Id = 22,
+                            Initials = "RO",
+                            Name = "Rondônia"
+                        },
+                        new
+                        {
+                            Id = 23,
+                            Initials = "RR",
+                            Name = "Roraima"
+                        },
+                        new
+                        {
+                            Id = 24,
+                            Initials = "SC",
+                            Name = "Santa Catarina"
+                        },
+                        new
+                        {
+                            Id = 25,
+                            Initials = "SP",
+                            Name = "São Paulo"
+                        },
+                        new
+                        {
+                            Id = 26,
+                            Initials = "SE",
+                            Name = "Sergipe"
+                        },
+                        new
+                        {
+                            Id = 27,
+                            Initials = "TO",
+                            Name = "Tocantins"
+                        });
+                });
+
             modelBuilder.Entity("M2P2_DEVinCar.Models.User", b =>
                 {
                     b.Property<int>("Id")
