f045bfe704b5a65390c58b81417f244f30d971ae
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-08-21 12:37:05 -0300

    feat: add PostAddress into StatesController

--- M2P2-DEVinCar/Controllers/StatesController.cs
+++ M2P2-DEVinCar/Controllers/StatesController.cs
@@ -71,6 +71,69 @@ namespace M2P2_DEVinCar.Controllers
             }
         }
 
+        /// <summary>
+        /// Adiciona um Endereço no banco de dados
+        /// </summary>
+        /// <param name="stateId">Id do Estado</param>
+        /// <param name="cityId">Id da Cidade</param>
+        /// <param name="addressDto">Dto do Endereço contento Id da cidade, rua, numero, cep e complemento(opcional)</param>
+        /// <returns>Retorna Endereço inserido com sucesso no banco de dados</returns>
+        /// <response code="201">Endereço inserido com sucesso</response>
+        /// <response code="400">O Id da cidade ou Id do estado não correspondem</response>
+        /// <response code="404">ID de cidade ou estado inválidos</response>
+        /// <response code="500">Ocorreu exceção durante a operação</response>
+        [HttpPost("{stateId}/city/{cityId}/address")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> PostAddress(int stateId, int cityId, [FromBody] CreateAddressDto addressDto)
+        {
+            try
+            {
+                bool stateIsValid = await _context.States
+                    .AnyAsync(state => state.Id == stateId);
+
+                if (stateIsValid == false) 
+                    return NotFound();
+
+                bool cityIsValid = await _context.Cities
+                    .AnyAsync(city => city.Id == cityId);
+
+                if (cityIsValid == false) 
+                    return NotFound();
+
+                var city = await _context.Cities.FirstOrDefaultAsync(city => city.Id == cityId);
+                bool cityAndStateMatch = city.StateId == stateId;
+
+                if (cityAndStateMatch == false)
+                    return BadRequest();
+
+                Address address = new()
+                {
+                   CityId = cityId,
+                   Street = addressDto.Street,
+                   Number = addressDto.Number,
+                   Cep = addressDto.Cep,
+                   Complement = addressDto.Complement
+                };
+
+                _context.Addresses.Add(address);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation($"Controller:{nameof(StatesController)}-Method:{nameof(PostAddress)}");
+
+
+                return StatusCode(201,new { Id = address.Id });
+
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, $"Controller:{nameof(StatesController)}-Method:{nameof(PostAddress)}");
+                return StatusCode(500);
+            }
+        }
+
         /*
         ● Caso não exista nenhum registro de Estado com o stateId enviado, deve ser retornado o Status de Erro 404 (Not Found)
         ● Caso exista alguma outra cidade criada nesse estado com o mesmo nome enviado,
