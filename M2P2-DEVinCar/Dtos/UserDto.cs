1e8047ffd447ccad2912a7bc8b6ec9be35d775ff
Vinicius Aloncio de Assunção <volutab617@gmail.com>
2022-08-22 15:30:28 -0300

    feat: Add UserDto

---
+++ M2P2-DEVinCar/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace M2P2_DEVinCar.Dtos {
+    public class UserDto {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Email { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
