10a50c3a0e3e7778d18282018a26f0c514621ae7
eduardo-borges-goncalves <alemaobrk1@gmail.com>
2022-08-22 15:36:52 -0300

    fix; update Users Table name

---
+++ M2P2-DEVinCar/Migrations/20220822183405_UpdateUser.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace M2P2_DEVinCar.Migrations
+{
+    public partial class UpdateUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_User_BuyerId",
+                table: "Sales");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_User_SellerId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_User",
+                table: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Email",
+                table: "User");
+
+            migrationBuilder.RenameTable(
+                name: "User",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Users_BuyerId",
+                table: "Sales",
+                column: "BuyerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_Users_SellerId",
+                table: "Sales",
+                column: "SellerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Users_BuyerId",
+                table: "Sales");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sales_Users_SellerId",
+                table: "Sales");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "User");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_User",
+                table: "User",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_User_BuyerId",
+                table: "Sales",
+                column: "BuyerId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sales_User_SellerId",
+                table: "Sales",
+                column: "SellerId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
