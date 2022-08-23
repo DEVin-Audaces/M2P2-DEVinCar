4c03dd4cda60b68736302bd247174233190871e3
Vinicius Aloncio de Assunção <volutab617@gmail.com>
2022-08-22 21:23:43 -0300

    feat: Add delete car method

--- M2P2-DEVinCar/Controllers/CarsController.cs
+++ M2P2-DEVinCar/Controllers/CarsController.cs
@@ -1,16 +1,18 @@
 ﻿using M2P2_DEVinCar.Context;
 using Microsoft.AspNetCore.Mvc;
 using M2P2_DEVinCar.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace M2P2_DEVinCar.Controllers {
     [Route("api/car")]
     [ApiController]
     public class CarsController : ControllerBase {
+        private readonly ILogger<UsersController> _logger;
         private DEVInCarContext _context;
 
-        public CarsController(DEVInCarContext context) {
+        public CarsController(DEVInCarContext context, ILogger<UsersController> logger) {
             _context = context;
+            _logger = logger;
         }
 
         /*[HttpGet]
@@ -51,21 +53,17 @@ namespace M2P2_DEVinCar.Controllers {
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Post([FromBody] Car car)
-        {
+        public async Task<IActionResult> Post([FromBody] Car car) {
 
-            try
-            {
+            try {
 
                 bool carExist = _context.Cars.Any(x => x.Name == car.Name);
-                if (carExist)
-                {
+                if (carExist) {
                     return StatusCode(400);
                 }
 
                 bool priceValid = car.SuggestedPrice > 0;
-                if (!priceValid)
-                {
+                if (!priceValid) {
                     return StatusCode(400);
                 }
 
@@ -75,16 +73,49 @@ namespace M2P2_DEVinCar.Controllers {
                 return CreatedAtAction("get", new { id = car.Id }, car);
 
             }
-            catch
-            {
+            catch {
                 return StatusCode(500);
             }
 
         }
 
-        /*
+
+        /// <summary>
+        /// Inserir aluno
+        /// </summary>
+        /// <param name="car"></param>
+        /// <returns>Retorna carro inserido</returns>
+        /// <response code = "204">Carro deletado</response>
+        /// <response code = "404">Deleção não realizada</response>
+        /// /// <response code = "400">Servidor não pode continuar</response>
+        /// <response code = "500">Erro execução</response>
         [HttpDelete("{id}")]
-        public void Delete(int id) {
-        }*/
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int carid) {
+            try {
+                var car = await _context.Cars.FirstOrDefaultAsync(x => x.Id == carid);
+                var salecar = await _context.SaleCars.FirstOrDefaultAsync(x => x.Id == carid);
+                if (car is null) {
+                    _logger.LogInformation($"Controller: {nameof(CarsController)} - Method: {nameof(Delete)} - ID: {carid}");
+                    return NotFound();
+                }
+                if(salecar is not null) {
+                    _logger.LogInformation($"Controller: {nameof(CarsController)} - Method: {nameof(Delete)} - ID: {carid}");
+                    return BadRequest();
+                }
+
+                _context.Cars.Remove(car);
+                await _context.SaveChangesAsync();
+                _logger.LogInformation($"Controller: {nameof(CarsController)} - Method: {nameof(Delete)} - ID: {carid}");
+                return StatusCode(204);
+            }
+            catch (Exception e) {
+                _logger.LogError(e, $"Controller: {nameof(CarsController)} - Method: {nameof(Delete)} - ID: {carid}");
+                return StatusCode(500);
+            }
+        }
     }
 }
